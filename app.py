d396ae20a73b18c1dbcef78f82c97e3fd0cc78ec
inetke <ineta.keryte@gmail.com>
2026-02-19 13:34:28 +0000

    class dict

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 
 model = load(open('src/random-forest-diabetes.pkl', 'rb'))
 
-class_dict = {'0': 'You are not diabetic',
-              '1': 'You are diabetic'}
+class_dict = {'0': 'You are not diabetic ✅',
+              '1': 'You are diabetic ⚠️'}
 
 st.title('🩺 Diabetes - Model prediction')
 st.markdown("""Power by: [Ineta Keryte]()""")
